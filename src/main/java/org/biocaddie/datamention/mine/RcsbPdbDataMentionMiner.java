32dfca06cd43172e002c887806de57c6a2db34df
Peter Rose <pwrose@ucsd.edu>
2015-08-30 13:16:51 -0700

    Updated documentation

--- src/main/java/org/biocaddie/datamention/mine/RcsbPdbDataMentionMiner.java
+++ src/main/java/org/biocaddie/datamention/mine/RcsbPdbDataMentionMiner.java
@@ -2,6 +2,7 @@ package org.biocaddie.datamention.mine;
 
 import java.io.IOException;
 import java.sql.Date;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -21,18 +22,48 @@ import org.rcsb.spark.util.SparkUtils;
 
 import scala.Tuple2;
 
+/**
+ * This class processes local copies of PubMedCentral Open Access subset articles downloaded from NCBI
+ * (see http://www.ncbi.nlm.nih.gov/pmc/tools/ftp/) and extracts sentences with PDB data mentions. The
+ * data mentions are saved as Apache Spark DataFrame in the Parquet file format 
+ * (http://spark.apache.org/docs/latest/sql-programming-guide.html).
+ * 
+ * It first processes the articles in the tar balls (articles.A-B.tar.gz, ..,
+ * articles.O-Z.tar.gz), which contain articles in the .nxml format. Then it processes articles 
+ * the tar balls (articles.txt.0-9A-B.tar.gz, .., articles.txt.O-Z.tar.gz), which contain extracted
+ * full text articles. It only extracts those articles that are not been found in the .nxml format files.
+ * 
+ * PMC OC is updated every Saturday. To get the latest data rerun this application
+ * every Saturday.
+ * 
+ * This application can be run in two modes:
+ * 
+ * 1. Full update mines the the text file from scratch
+ * 
+ *      RcsbPdbDataMentionMiner inputDirectory outputDirectory
+ *      
+ * 2. Incremental update by specifying the date of the last update
+ * 
+ *     RcsbPdbDataMentionMiner inputDirectory outputDirectory -lastUpdated YYYY-MM-DD
+ *     
+ * The input directory must contain the tar balls downloaded from PubMedCentral. No other tar balls
+ * should be present in the input directory.
+ * 
+ * @author Peter Rose
+ *
+ */
 public class RcsbPdbDataMentionMiner
 {
 	private static final String PMC_FILE_METADATA = "PmcFileMetadata.parquet";
 	private static final String OUTPUT_FILE_NAME = "PdbDataMentions.parquet";
 	private static final String OUTPUT_FORMAT = "parquet";
 	
-//	private static final int NUM_THREADS = 4;
 	private static final int NUM_TASKS = 3;
-	private static final int BATCH_SIZE = 20000;
+	private static final int BATCH_SIZE = 10000;
 	
 	private String pmcMetadataFileName;
 	private String outputFileName;
+	private String outputFormat;
 	private Date lastUpdated;
 	private List<String> pmcFileList;
 	
@@ -48,29 +79,35 @@ public class RcsbPdbDataMentionMiner
 		long start = System.nanoTime();
 		
 		RcsbPdbDataMentionMiner miner = new RcsbPdbDataMentionMiner();
-		
-		miner.parseCommandLine(args);
-		
-		miner.initializeSpark();
-		
-		miner.loadPmcFileMetaData();
-		
-		miner.createUpdateMap();
-		
-		miner.mine();
-		
-		miner.closeSpark();
+		miner.writeDataMentions(args);
 		
 		long time = System.nanoTime() - start;
 
-		System.out.println("time: " + time/1E9 + " s");
+		System.out.println("Time: " + time/1E9 + " s");
+	}
+	
+	public void writeDataMentions(String[] args) throws Exception {
+		parseCommandLine(args);
+
+		initializeSpark();
+
+		if (isIncrementalUpdate()) {
+			loadPmcFileMetaData();
+			createUpdateMap();
+		}
+
+		mine();
+
+		closeSpark();
 	}
 	
 	private void parseCommandLine(String[] args) throws IOException {
-//		System.out.println("Usage: RcsbPdbDataMentionMiner pmcFileList.parquet pmcFileNames ...  outputFile.parquet [-lastUpdated date]");
-//        System.out.println(" latUpdated: ");
-//        throw new IllegalArgumentException("Invalid command line arguments");
-        
+		if (! (args.length == 2 || args.length == 4) ) {
+			System.out.println("Usage: " +
+					"RcsbPdbDataMentionMiner inputdirectory outputdirectory [-lastUpdated YYYY-MM-DD");
+			throw new IllegalArgumentException("Invalid command line arguments: " + Arrays.toString(args));
+		}
+ 
 		String inputDirectory = args[0];
 		List<String> inputFiles = PmcTarBallReader.getTarBallFileNames(inputDirectory);	
 		setPmcFileList(inputFiles);
@@ -80,16 +117,17 @@ public class RcsbPdbDataMentionMiner
 		setOutputFileName(outputFileName);
 		String pmcFileMetadata = outputDirectory + "/" + PMC_FILE_METADATA;
 		setPmcMetadataFileName(pmcFileMetadata);
+		setOutputFormat(OUTPUT_FORMAT);
+		
+		System.out.println("PMC File List   : " + getPmcFileName());
+		System.out.println("Data Mention File Name: " + getOutputFileName());
 		
 		// check for incremental update since the last update date 
 		if (args.length == 4 && args[2].equals("-lastUpdated")) {
 			setLastUpdated(Date.valueOf(args[3]));
-		}
-		
-		System.out.println("Last updated    : " + getLastUpdated());
-		System.out.println("PMC File List   : " + getPmcFileName());
-		System.out.println("PMC File Names  : " + getPmcFileList());
-		System.out.println("Output File Name: " + getOutputFileName());
+			System.out.println("Last updated       : " + getLastUpdated());
+			System.out.println("PMC File metadata  : " + getPmcFileList());
+		}	
 	}
 	
 	public String getPmcFileName() {
@@ -108,12 +146,24 @@ public class RcsbPdbDataMentionMiner
 		this.outputFileName = outputFileName;
 	}
 
+	public String getOutputFormat() {
+		return outputFormat;
+	}
+
+	public void setOutputFormat(String outputFormat) {
+		this.outputFormat = outputFormat;
+	}
+
 	public Date getLastUpdated() {
 		return lastUpdated;
 	}
 
 	public void setLastUpdated(Date lastUpdated) {
 		this.lastUpdated = lastUpdated;
+	}
+	
+	public boolean isIncrementalUpdate() {
+		return this.lastUpdated != null;
 	}
 
 	public List<String> getPmcFileList() {
@@ -136,6 +186,7 @@ public class RcsbPdbDataMentionMiner
 	{		
         boolean firstBatch = true;
         int threads = sparkContext.defaultParallelism();
+        
         System.out.println("Number of threads: " + threads);
 
 		int count = 0;
@@ -143,23 +194,30 @@ public class RcsbPdbDataMentionMiner
 		for (String fileName: pmcFileList) {
 			System.out.println("Processing: " + fileName);
 
+			// read articles in batches to avoid out of memory errors.
 			PmcTarBallReader reader = new PmcTarBallReader(fileName, BATCH_SIZE);
+			
 			reader.setUpdateMap(updateMap);
 			reader.setLastUpdated(lastUpdated);
 			
+			// read batches and find data mentions
 			while (reader.hasNext()) {
 				List<Tuple2<String, byte[]>> tuples = reader.getNext();
 				
 				tuples = trimRedundantRecords(tuples);
 				count += tuples.size();
 				
+				// create distributed dataset for parallel processing
 				JavaPairRDD<String, byte[]> data = sparkContext.parallelizePairs(tuples, threads*NUM_TASKS);
 				
+				// keep records that contain PDB ID (match PDB regex) and create a separate
+				// data mention record for every PDB ID found in an article
 				JavaRDD<DataMentionRecord> records = data
 				.filter(new PdbRegExFilter())
 				.flatMap(new PdbDataMentionMapper());
 				
-				// Apply a schema to an RDD of JavaBeans and register it as a table.
+				// convert RDD to a Spark DataFrame using the DataMentionRecord Java Bean as the
+				// schema definition.
 				DataFrame dataMentions = sqlContext.createDataFrame(records, DataMentionRecord.class);
 				dataMentions = SparkUtils.toRcsbConvention(dataMentions);
 				
@@ -169,11 +227,11 @@ public class RcsbPdbDataMentionMiner
 					dataMentions.printSchema();
 					dataMentions.show();
 						
-					dataMentions.write().mode(SaveMode.Overwrite).parquet(getOutputFileName());
+					dataMentions.coalesce(1).write().format(getOutputFormat()).mode(SaveMode.Overwrite).save(getOutputFileName());
 					firstBatch = false;
 				} else {
 					System.out.println("appending records");
-					dataMentions.write().mode(SaveMode.Append).parquet(getOutputFileName());
+					dataMentions.coalesce(1).write().format(getOutputFormat()).mode(SaveMode.Append).save(getOutputFileName());
 				}
 			}
 		}
@@ -181,13 +239,20 @@ public class RcsbPdbDataMentionMiner
 		System.out.println("Processed records: " + count);
 	}
 	
+/**
+ * Trims redundant records (same file name without extension). This happens since many articles are available in 
+ * .nxml and .txt format. We only keep the unique .txt files. These are articles that are only available in 
+ * .pdf format that have been converted to .txt format.
+ * 
+ * @param tuples file name, article pairs
+ * @return unique list of file name, article pairs
+ */
 	private List<Tuple2<String, byte[]>> trimRedundantRecords(List<Tuple2<String, byte[]>> tuples) {
 		System.out.println("Original size: " + tuples.size());
 		for (Iterator<Tuple2<String, byte[]>> iter = tuples.iterator(); iter.hasNext();) {
 			Tuple2<String, byte[]>  tuple = iter.next();
 			if (uniqueFileNames.contains(tuple._1)) {
 				iter.remove();
-//				System.out.println("removing: " + tuple._1);
 			} else {
 				uniqueFileNames.add(tuple._1);
 			}
@@ -205,7 +270,6 @@ public class RcsbPdbDataMentionMiner
 	}
 	
 	private void loadPmcFileMetaData() {
-		// pmcFileMetadata: pmcId, pmId, fileName, lastUpdated
 		pmcFileMetaData = sqlContext.read().parquet(pmcMetadataFileName);
 	}
 	
